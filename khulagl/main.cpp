77734402208df66b2f29ddb09569abe36cbb1fcc
naduhrin <naduhrin78@gmail.com>
2023-04-02 14:22:01 +0530

    refactored window logic

--- khulagl/main.cpp
+++ khulagl/main.cpp
@@ -39,8 +39,6 @@ float lastFrame = 0.0f;
 
 int main(void)
 {
-    GLFWwindow* window;
-    
     /* Initialize the glfw library */
     if (!glfwInit())
         return -1;
@@ -52,7 +50,8 @@ int main(void)
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "KhulaGL", NULL, NULL);
+
     if (!window)
     {
         glfwTerminate();
